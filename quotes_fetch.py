352c36dcdf9a544c9eb3bee41b417ccfdf694441
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-06-24 23:41:35 +0530

    Update quotes_fetch.py

--- quotes_fetch.py
+++ quotes_fetch.py
@@ -1,37 +1,35 @@
-import requests
 import streamlit as st
+import requests
 
-url = "https://quotes15.p.rapidapi.com/quotes/random/"
-
-headers = {
-    "X-RapidAPI-Key": "2dc054a387msh10f1aa3417dd608p1cf866jsn38be8f35af2d",
-    "X-RapidAPI-Host": "quotes15.p.rapidapi.com"
-}
+# Replace 'YOUR_BOT_TOKEN' with your Telegram bot token
+bot_token = '6010408512:AAHpLwF_PlsmHfoTAJkCIuKunopcVCB4yXw'
+url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
 num_quotes = 3  # Number of quotes to fetch and display
 
 
-def fetch_quotes():
-    quotes = []
+def send_message(chat_id, text):
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code != 200:
+        st.error(f"Failed to send message: {response.text}")
+
+
+def quote():
     for _ in range(num_quotes):
-        response = requests.get(url, headers=headers)
+        response = requests.get("https://quotes15.p.rapidapi.com/quotes/random/",
+                                headers={"X-RapidAPI-Key": "2dc054a387msh10f1aa3417dd608p1cf866jsn38be8f35af2d",
+                                         "X-RapidAPI-Host": "quotes15.p.rapidapi.com"})
         data = response.json()
+
         if "content" in data:
-            quote = data["content"]
-            quotes.append(quote)
-    return quotes
+            quote_text = data["content"]
+            send_message(chat_id="5809290032", text=quote_text)
 
 
 def main():
-    st.title("Random Quote Bot")
-    st.write("Welcome! I'm a chatbot that can generate random quotes.")
-
-    quotes = fetch_quotes()
-
-    if st.button("Generate Quote"):
-        for quote in quotes:
-            st.write(quote)
-
-
 if __name__ == "__main__":
     main()
